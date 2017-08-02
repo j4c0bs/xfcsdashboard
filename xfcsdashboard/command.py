ea76544a798b993cb480e079d501078f4f6aa6b2
x <j4c0bs@users.noreply.github.com>
2017-08-01 21:50:34 -0500

    Initial commit.

---
+++ xfcsdashboard/command.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from xfcsdashboard import dashboard
+
+# ------------------------------------------------------------------------------
+def parse_arguments():
+    """Parse command line arguments."""
+
+    parser = argparse.ArgumentParser(prog='xfcsdashboard', description='Plots FCS Metadata')
+
+    parser.add_argument(
+        '--input', '-i', nargs='+', type=argparse.FileType('rb'),
+        metavar='<fcs_metadata.csv>', help='FCS metadata csv files to plot.')
+
+    return parser.parse_args()
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    args = parse_arguments()
+    files = [infile.name for infile in args.input]
+    dashboard.plot_csv(files)
